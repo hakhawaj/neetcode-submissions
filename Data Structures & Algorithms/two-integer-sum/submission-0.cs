088349bc6d43fe76585432ae944df386aaca25b0
hakhawaj <hakhawaj@uwaterloo.ca>
2026-05-17 18:35:52 -0400

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var index = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int diff = target - nums[i];
+            if (index.ContainsKey(diff)) return [index[diff], i];
+            index.Add(nums[i], i);
+        }
+
+        return [0, 0];
+    }
+}
